795168df1c66599ffc7d3fbab1edcd934dafa079
Nevzat YAVUZCAN <nevzat.yavuzcan@etiya.com>
2020-02-20 01:08:31 +0300

    With Scrolling

--- src/main/java/com/openhere/Main.java
+++ src/main/java/com/openhere/Main.java
@@ -1,12 +1,11 @@
 package com.openhere;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NotFoundException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.awt.*;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -40,14 +39,14 @@ public class Main {
                 List<WebElement> webElementIlcelist= web.findElements(By.cssSelector("li [data-level='town']"));
               //  List<WebElement> webElements =new ArrayList<WebElement>();
                 for (int i =1 ; i<=webElementIlcelist.size(); i++){
-                  //  webElements.add(webElementIlcelist.get(i));
+                  JavascriptExecutor js = (JavascriptExecutor) web;
+                  WebElement element =web.findElement(By.xpath("//*[@id=\"searchResultLeft-address\"]/dl/dd/ul/li[2]/div/div[3]/div/div[1]"));
+                  js.executeScript("arguments[0].setAttribute('style', 'padding: 0px; top: -"+i*18+"px; left: 0px; width: 209px;')",element);
+
                     webElementIlcelist.get(i).click();
-                 /*   webElementIlcelist.get(33).click();
 
-                    JavascriptExecutor js = (JavascriptExecutor) web;
 
-// Scroll inside web element vertically (e.g. 100 pixel)
-                    js.executeScript("arguments[0].scrollTop = arguments[1];",web.findElement(By.className("address-pane active")), 100);*/
+
                     web.findElement(By.className("address-overlay")).click();
                     Thread.sleep(1000);
                     web.findElement(By.linkText("Mahalle")).click();
@@ -57,7 +56,11 @@ public class Main {
                     Thread.sleep(1500);
                     web.findElement(By.cssSelector("dd > ul >li[data-address='town']")).click();
                     Thread.sleep(1500);
-                    webElementIlcelist.get(i).click();
+                  JavascriptExecutor jsx = (JavascriptExecutor) web;
+                  WebElement elementx =web.findElement(By.xpath("//*[@id=\"searchResultLeft-address\"]/dl/dd/ul/li[2]/div/div[3]/div/div[1]"));
+                  jsx.executeScript("arguments[0].setAttribute('style', 'padding: 0px; top: -"+i*18+"px; left: 0px; width: 209px;')",elementx);
+
+                  webElementIlcelist.get(i).click();
 
                     Thread.sleep(3000);
                     continue;
